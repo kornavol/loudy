c2b2b550bf9f2dcc5616378cbf13089816feaee0
kornavol <volosh.andrey@gmail.com>
2021-12-14 21:33:06 +0100

    add basic playbar

--- src/components/MusicPlayer/Playbar/Playbar.tsx
+++ src/components/MusicPlayer/Playbar/Playbar.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import "./style.css";
 
-const Playbar : FC = ()  => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const Playbar: FC = () => {
+  return (
+    <div id="playbar">
+    </div>
+  );
+};
 
 export default Playbar;
