498e583817b425e3d698cef09d5e29dba1505311
kornavol <volosh.andrey@gmail.com>
2021-12-14 23:54:18 +0100

    add play pause button to music list

--- src/components/MusicPlayer/Content/Content.tsx
+++ src/components/MusicPlayer/Content/Content.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState, useContext  } from 'react'
+import React, { FC, useEffect, useState, useContext } from 'react'
 import { StoreContext } from '../MusicPayer'
 
 import './style.css'
@@ -6,16 +6,17 @@ import { loadSongs } from '../../../api'
 
 const Content: FC = () => {
   const { state, dispatch } = useContext(StoreContext)
-  
+
   const [list, setList] = useState<any>([])
   const [isFavorite, setIsFavorite] = useState(false)
+  const [playVisibleId, setPlayVisibleId] = useState('')
 
   useEffect(() => {
     const fetchSongs = async () => {
       const result = await loadSongs()
       if (result && result.status === 200) {
-        console.log(result.data);
-        
+        console.log(result.data)
+
         setList(result.data)
       }
     }
@@ -38,15 +39,31 @@ const Content: FC = () => {
           {list &&
             list.map((song: any) => {
               const { id, name, music_file_path, artist_name } = song
-              
+
               return (
                 <tr key={id}>
-                  <td>
+                  <td style={{ width: 75, paddingLeft: 5 }}>
                     <Favorite isFavorite={isFavorite} />
+
+                    <span style={{ marginRight: 20 }} />
+
+                    <PlayPause
+                      playing={state.playing}
+                      isCurrentSong={state.id === id}
+                      visible={playVisibleId === id}
+                    />
+
+                    <span style={{ marginRight: 20 }} />
                   </td>
                   <td
-                  className='songName'
-                   onClick={()=>dispatch({type:'PLAY', path:music_file_path})}>{name}</td>
+                    className="songName"
+                    onClick={() => {
+                      dispatch({ type: 'PLAY', path: music_file_path, id })
+                      setPlayVisibleId(id)
+                    }}
+                  >
+                    {name}
+                  </td>
                   <td>{artist_name}</td>
                 </tr>
               )
@@ -65,4 +82,27 @@ const Favorite = ({ isFavorite }) => {
   ) : (
     <i className="fa fa-heart-o" />
   )
+}
+
+const PlayPause = ({ playing, isCurrentSong, visible }) => {
+  const { dispatch } = useContext(StoreContext)
+  const style: any = { visibility: visible ? 'visible' : 'hidden' }
+
+  if (isCurrentSong && playing) {
+    return (
+      <i
+        className="fa fa-pause"
+        onClick={() => dispatch({ type: 'PAUSE' })}
+        style={style}
+      />
+    )
+  } else {
+    return (
+      <i
+        className="fa fa-play"
+        onClick={() => dispatch({ type: 'PLAY' })}
+        style={style}
+      />
+    )
+  }
 }
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,22 +1,27 @@
 interface IInitialState {
     music_file_path: string
     playing: boolean
+    id:string
 }
 
 export const initialState: IInitialState = {
+    id:"",
     music_file_path: "",
     playing: false,
 }
 
 
-export const reducer = (state:IInitialState = initialState, action) => {
+export const reducer = (state: IInitialState = initialState, action) => {
     switch (action.type) {
         case 'PLAY':
             return {
                 ...state,
-                playing:true,
-                music_file_path: action.path || state.music_file_path
+                playing: true,
+                music_file_path: action.path || state.music_file_path, 
+                id:action.id || state.id
             }
+        case 'PAUSE':
+            return { ...state, playing: false }
         default:
             return state
     }
